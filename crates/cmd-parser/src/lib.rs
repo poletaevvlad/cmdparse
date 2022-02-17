5ec6e7826699908730be6710a9e72e92873299ae
Vlad Poletaev <poletaev.vladislav@gmail.com>
2022-02-18 00:32:48 +0300

    Added crate-level documentation

--- crates/cmd-parser/src/lib.rs
+++ crates/cmd-parser/src/lib.rs
@@ -1,15 +1,157 @@
+//! `cmd_parser` is, as the name suggests, parses user commands into arbitrary Rust types
+//!
+//! Genrealy, this crate can be viewed as a data deserialization framework. It defines a syntax
+//! designed to be easy to entered interactively and includes utilities for transforming the input
+//! in this format into arbitrary Rust types as well as automatically completing incorrect user.
+//!
+//! It is **not suitable for parsing command line arguments** even though the syntax it supports is
+//! fairly similar to what those would look like. Instead it was designed to be used for parsing
+//! commands entered interactively inside the application. Of cource, you are not limited to this
+//! use case and free to use `cmd_parser` as a generic data deserialization framework in any way
+//! you like.
+//!
+//! # Example
+//!
+//! Let's consider the following example. It defines a struct `MailSendCommand` and derives
+//! [`Parsable`] trait for it. This is enough to be able to parse this sruct.
+//!
+//! ```
+//! use cmd_parser::{Parsable, parse};
+//!
+//! #[derive(Debug, PartialEq, Eq, Parsable)]
+//! struct MailSendCommand {
+//!    text: String,
+//!    #[cmd(attr(subject), default = "\"no subject\".to_string()")]
+//!    subject: String,
+//!    #[cmd(attr(to))]
+//!    to: Vec<String>,
+//! }
+//!
+//! # fn main() -> Result<(), cmd_parser::error::ParseError<'static>> {
+//! let input = "\"Hello, world\" --to user1@example.com user2@example.com --subject Greeting";
+//! let result = parse::<_, MailSendCommand>(input, ())?;
+//! assert_eq!(result, MailSendCommand {
+//!     text: "Hello, world".to_string(),
+//!     subject: "Greeting".to_string(),
+//!     to: vec!["user1@example.com".to_string(), "user2@example.com".to_string()],
+//! });
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! This example demonstrates several features of `cmd_parser`:
+//!
+//!  * Parsing functionality can be automatically derived for an arbitrary struct or enum as long
+//!    as the inner types are [`Parsable`] or there is an apporpriate [`Parser`] for them. (To learn
+//!    about the distinction between parsable and parser read documentation for these traits)
+//!  * Derived parser is configurable: you may make fields either required or optional. Optional
+//!    fields can be specified via a name attribute (`--` token). They can have a default value
+//!    explicitly specified (see default attribute on the `subject` field) or not (`to` field
+//!    defaults to an empty vector, as per its [`Default`] implementation)
+//!  * Parsable values can contain nested parsable values: `MailSendCommand` is parsable, it
+//!    contains a `Vec` which is parsable and in repeatedly parses `String`s that are parsable.
+//!    Note how `cmd_parser` recognized that the list of email addresses finished when it
+//!    encountered the attribute that nither `String` nor `Vec` recognzies.
+//!
+//! `cmd_parser` can generate completion suggestions:
+//!
+//! ```
+//! # use cmd_parser::{Parsable, parse};
+//! use cmd_parser::complete;
+//! use std::collections::BTreeSet;
+//!
+//! # #[derive(Debug, PartialEq, Eq, Parsable)]
+//! # struct MailSendCommand {
+//! #    text: String,
+//! #    #[cmd(attr(subject), default = "\"no subject\".to_string()")]
+//! #    subject: String,
+//! #    #[cmd(attr(to))]
+//! #    to: Vec<String>,
+//! # }
+//! #
+//! # fn main() -> Result<(), cmd_parser::error::ParseError<'static>> {
+//! let suggestions = complete::<_, MailSendCommand>("\"Hello, world\" --", ());
+//! assert_eq!(suggestions, BTreeSet::from(["to".into(), "subject".into()]));
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! It also supports parsing enums. In case of enum it expects a discriminator (automatically
+//! converted into kebab-case by the [`Parsable`] derive macro):
+//!
+//! ```
+//! use cmd_parser::{parse, Parsable};
+//!
+//! #[derive(Debug, PartialEq, Eq, Parsable)]
+//! enum Priority {
+//!    High,
+//!    Medium,
+//!    Low,
+//! }
+//!
+//! impl Default for Priority {
+//!     fn default() -> Self {
+//!         Priority::Medium
+//!     }
+//! }
+//!
+//! #[derive(Debug, PartialEq, Eq, Parsable)]
+//! enum Command {
+//!     AddTask(String, #[cmd(attr(priority))] Priority),
+//!     Remove(usize),
+//! }
+//!
+//! # fn main() -> Result<(), cmd_parser::error::ParseError<'static>> {
+//! assert_eq!(
+//!     parse::<_, Command>("add-task parse-all-commands", ())?,
+//!     Command::AddTask("parse-all-commands".to_string(), Priority::Medium),
+//! );
+//! assert_eq!(
+//!     parse::<_, Command>("add-task enjoy-your-day --priority high", ())?,
+//!     Command::AddTask("enjoy-your-day".to_string(), Priority::High),
+//! );
+//! assert_eq!(parse::<_, Command>("remove 1", ())?, Command::Remove(1));
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! # Syntax
+//!
+//! The syntax that `cmd_parser` supports is fairly minimal. The parsing machinery sees the input
+//! as a sequence of tokens. Token is any sequence of craracters separated by whitespaces. If you
+//! wish to include a whitespace in the token, you may enclose any substring of the input into a
+//! pair of quotes (either double or singular); `cmd_parser` supports escaping quotes inside quoted
+//! tokens with a slash (`\`).
+//!
+//! Input can contain a comment begining with an octothorp (`#`). Octothorps within quoted tokens
+//! are not considered to begin a comment.
+//!
+//! The meaning of the token and attributes are highly specific to each parser. Generally each
+//! parser consumes tokens sequentially until each required field's value is filled. It also
+//! handles attributes in any order and at arbitrary positions.
+//!
+//! Due to the nature of the commands syntax, parsing can seem ambiguous. For example, `cmd_parser`
+//! can parse nested structs such as `Vec<Vec<u32>>`. It may be confusing to the end user, how
+//! would a sequence of numbers be interpreted (they all will be put in the only item of the outer
+//! vector). It is best to design your command to be simple and avoid highly nested structures for
+//! the better user experience. In some cases, complexity is unavoidable. In such situations, users
+//! may group tokens belonging to the same data structure with parenthesis (`(` and `)`). This way
+//! users can express a value `vec![vec![1, 2], vec![3, 4, 5]]` as `(1 2) (3 4 5)`.
+//!
+//! More details about how the tokenization and the parsing algorithm are documented in the
+//! [`tokens`] module's and [`Parser`] trait's documentation.
+
 pub mod error;
 pub mod parsers;
 pub mod testing;
 pub mod tokens;
 
-use std::borrow::Cow;
-use std::collections::BTreeSet;
-
 #[doc(hidden)]
 pub use cmd_parser_derive::Parsable;
 use error::ParseError;
 use error::ParseFailure;
+use std::borrow::Cow;
+use std::collections::BTreeSet;
 use tokens::TokenStream;
 
 /// The result value returned by the individual parsers
